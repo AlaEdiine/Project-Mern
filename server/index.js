7d011b86fd88aca8bba4e8f69e40680dbfd9e77c
Ala.Eddine <143133176+AlaEdiine@users.noreply.github.com>
2023-08-30 23:42:34 +0100

    Update index.js

--- server/index.js
+++ server/index.js
@@ -14,31 +14,6 @@ var app = express()
 
 
 
-var allowedOrigins = ['http://someorigin.com',
-                      'http://localhost:3001',
-                      'https://frontends-0n02.onrender.com',
-                      'http://localhost:1234',
-                      'http://localhost:3000'];
-
-app.use(cors({
-
-  origin: function(origin, callback){
-    // allow requests with no origin
-    // (like mobile apps or curl requests)
-    if(!origin) return callback(null, true);
-    if(allowedOrigins.indexOf(origin) === -1){
-      var msg = 'The CORS policy for this site does not ' +
-                'allow access from the specified Origin.';
-      return callback(new Error(msg), false);
-    }
-    return callback(null, true);
-  },
-
-  exposedHeaders: ['Content-Length', 'X-Foo', 'X-Bar'],
-
-  credentials: true,
-}));
-
 
 
 // pour toutes les cors
